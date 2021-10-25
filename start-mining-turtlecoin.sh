792723f410d09ed4493e612e0e560ffc18d83043
Epan88 <79555705+Epan88@users.noreply.github.com>
2021-10-26 03:32:42 +0700

    Create start-mining-turtlecoin.sh

---
+++ start-mining-turtlecoin.sh
@@ -0,0 +1,5 @@
+sudo apt update
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.1/SRBMiner-Multi-0-8-1-Linux.tar.xz
+tar xf SRBMiner-Multi-0-8-1-Linux.tar.xz
+cd SRBMiner-Multi-0-8-1
+./SRBMiner-MULTI --setup -c us.turtlecoin.herominers.com:1160 -u TRTLv1kFpeh37kezvpep4pBJsad6pEJKGcEmPV2piyM8MV4wFndkVPSSxsGLGbYEQWLiaveek8JdCdTEDNRCMbfADGEnhx5PfmU -p x --cpu 4
